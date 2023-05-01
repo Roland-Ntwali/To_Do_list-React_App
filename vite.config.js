a6c5d539513952cae30cc3017a5e76f4a0dc52ae
Roland-Ntwali <rolandntwali@gmail.com>
2023-05-01 22:35:28 +0200

    fix linters

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
+/* eslint-disable */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  plugins: [react()],
 });
